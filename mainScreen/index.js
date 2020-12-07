c3d1d242fcb8c1b2c6a1453cc5f91dae3d618345
Cody Lillywhite <codylillyw@gmail.com>
2020-12-06 19:55:56 -0500

    added websocket connection

--- mainScreen/index.js
+++ mainScreen/index.js
@@ -7,6 +7,14 @@ import mainScreenController from './mainScreenController.js';
 const controller = new mainScreenController();
 controller.mainScreenView.generateMap(24,12);
 controller.mainScreenView.initPlayer("../img/dragonborn.png",4,1);
+const ws = new WebSocket('ws://localhost:5500');
+ws.addEventListener('open', () => {
+    console.log('We are connected!');
+    ws.send('What is up?!')
+});
+
+
+//EXPERIMENTAL: zooming and getting background size.
 // controller.mainScreenView.allowZoom();
 // let imageSize = controller.mainScreenModel.getImageSize("../img/map1.jpg");
 // let elemSize = document.getElementById('grid');
