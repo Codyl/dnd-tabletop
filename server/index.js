43361df110ea3c8d5986d5a22671ac6c13f5e190
Cody Lillywhite <codylillyw@gmail.com>
2020-12-06 19:52:56 -0500

    added websocket connection

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port:5500 });
+
+wss.on('connection', ws => {
+    console.log('server logged a connection!');
+    ws.on('message',data => {
+        console.log(`Client has sent us: ${data}`);
+        ws.send(data.toUpperCase());
+    });
+    ws.on('close', () => {
+        console.log('Client has disconnected!');
+    });
+});
