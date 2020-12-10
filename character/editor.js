98f8ae6dc3ac23d85fc01f012a05af63f0e1e484
Cody Lillywhite <codylillyw@gmail.com>
2020-12-09 21:35:51 -0500

    fixed typo causing character edit to not bring up  data

--- character/editor.js
+++ character/editor.js
@@ -3,4 +3,4 @@ import characterController from './characterController.js';
 const myCharacterController = new characterController();
 document.getElementById('saveCharacter').addEventListener('click',()=>{myCharacterController.saveClick()});
 myCharacterController.uploadImage();
-if(window.location.search != "?newCharacter") myCharacterController.characterView.setFormDataValue();
+if(window.location.search != "?newCharacter") myCharacterController.characterView.setFormDataValue(true);
